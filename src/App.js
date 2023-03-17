d292392fd99e2a119255d346a16abe5a9ed207a9
Harsh Jha <harshjha19101997@gmail.com>
2023-03-17 13:06:17 -0700

    form completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
-// import From from './components/Form'
-import MainForm from './components/MainForm';
-// import FromOne from './components/FromOne';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
+import MainForm from './components/Mainform/MainForm';
+import Footer from './components/Footer/Footer';
+
+
+
 const App = () => {
   return (
     <div>
---
+++ src/components/Mainform/MainForm.jsx
@@ -0,0 +1,188 @@
+// import Button from "react-bootstrap/Button";
+import { React, useState, createElement } from "react";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { DateRangePicker, Button, Content } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import "./formStyle.scss";
+
+function MainForm() {
+
+  // set initial state for title, designation, tech skills, and description
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [techSkills, setTechSkills] = useState("");
+  const [description, setDescription] = useState("");
+  const [t,setT] = useState(1);
+  // set initial state for project fields as an array of one empty object
+  const [projects, setProjects] = useState([
+    { title: "", techStack: "", shortDescription: "" },
+  ]);
+
+  //for handling candidate name change
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  }
+  //for handling candidate designation change..
+  const designationChangeHandler = (e) => {
+    setDesignation(e.target.value);
+    console.log(designation);
+  }
+  //for handling skills change..
+  const skillsChangeHandler = (e) => {
+    setTechSkills(e.target.value);
+    console.log(techSkills);
+  }
+  //forhandling description change..
+  const descriptionChangeHandler = (e) => {
+    setDescription(e.target.value);
+    console.log(description);
+  };
+
+  // function to add a new project field
+  const handleAddProject = () => {
+    console.log("project added",t+1);
+    setProjects([
+      ...projects,
+      { title: "", techStack: "", shortDescription: "" },
+    ]);
+  };
+
+//when a new project added.. via plus button
+  const handleProjectChange = (event, index) => {
+    const { name, value } = event.target;
+    const updatedProjects = [...projects];
+    updatedProjects[index] = { ...updatedProjects[index], [name]: value };
+    setProjects(updatedProjects);
+  }
+
+  
+
+  const handleSubmit = async(event) =>
+  {
+    console.log("data is:",{
+      name
+      ,designation
+      ,techSkills
+      ,description
+      ,projects
+    })
+    event.preventDefault();
+    try{
+      const response = await axios.post("api here",{
+      name: name,
+      designation: designation,
+      techSkills: techSkills,
+      description: description,
+      projects:projects
+    })
+    console.log(response);
+    }catch(error){
+      console.log(error);
+    }
+  
+  }
+  
+
+  return (
+    <div>
+      {/* //this is the div for user profile */}
+      <div style={{ marginBottom: "9rem" }} className="mainFromdiv">
+        <h2>Your Profile</h2>
+        <form onSubmit={handleSubmit}>
+
+          {/* asking name of the candidate */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>name</Form.Label>
+            <Form.Control onChange={nameChangeHandler} value={name} type="email" placeholder="Name" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+
+          {/* asking designation of the candidate */}
+          <Form.Group className="mb-3" controlId="formBasictext">
+            <Form.Label>Designation</Form.Label>
+            <Form.Control onChange={designationChangeHandler} value={designation} type="text" placeholder="Designation" />
+          </Form.Group>
+
+
+          {/* asking tech skills of the candidate */}
+          <Form.Group className="mb-3" controlId="formBasictext">
+            <Form.Label>TechSkills</Form.Label>
+            <Form.Control onChange={skillsChangeHandler} value={techSkills} type="text" placeholder="TechSkills" />
+          </Form.Group>
+
+
+          {/* asking professional summary of the candidate */}
+          <Form.Group className="mb-3" controlId="formBasictext">
+            <Form.Label>Professional Summary</Form.Label>
+            <Form.Control onChange={descriptionChangeHandler} description={description} type="text" placeholder="Professional Summary" />
+          </Form.Group>
+
+
+          {/* asking details about the project now    */}
+          <div className="projectTitle">
+            {/* heading about the project */}
+            <h3 style={{ marginBottom: "2rem" }}>Project</h3>
+            {/* detail about the project */}
+
+
+            {
+              projects.map((project, index) => (
+
+                <div style={{ border: "1px" }} key={index}>
+                  <div>
+                  <label>Project:</label> <h6 style={{display:"inline",margin:"2rem 0 2rem 0"}}>{index+1}</h6>
+                  </div>
+                 <form>
+                 <Form.Group className="mb-3" controlId="formBasictext">
+                    {/* asking title of the project */}
+                    <Form.Label>Title:</Form.Label>
+                    <Form.Control name="title" value={project.title} onChange={(event) => handleProjectChange(event, index)} type="text" placeholder="Title" />
+                  </Form.Group>
+
+                  {/* asking duration of the candidate in the company */}
+                  {/* not mandatory */}
+                  <Form.Group className="mb-3" controlId="formBasictext">
+                    <Form.Label> Duration &nbsp;&nbsp;</Form.Label>
+                    <DateRangePicker name="date" />
+                  </Form.Group>
+
+                  {/* asking technical skills of the candiate */}
+                  <Form.Group className="mb-3" controlId="formBasictext">
+                    <Form.Label>TechStack</Form.Label>
+                    <Form.Control name="techStack" value={project.techStack} onChange={(event) => handleProjectChange(event, index)} type="text" placeholder="TechSkills" />
+                  </Form.Group>
+                  {/* short description about the candidate */}
+                  <Form.Group className="mb-3" controlId="formBasictext">
+                    <Form.Label>ShortDescription</Form.Label>
+                    <Form.Control name="shortDescription" value={project.shortDescription} onChange={(event) => handleProjectChange(event, index)} type="text" placeholder="Designation" />
+                  </Form.Group>
+                 </form>
+                
+                </div>
+              ))
+            }
+          </div>
+            <div style={{display:"flex",justifyContent:"space-between"}} >
+              
+              <div style={{display:"inline-block"}}>
+              <i onClick={handleAddProject} class="fas fa-plus fa-3x"></i>
+              </div>
+             
+            
+
+
+            <div  style={{display:"inline-block"}} className="submitBtn">
+              <button  type="submit" onClick={handleSubmit}  appearance="primary">Submit</button>
+            </div>
+
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+}
+export default MainForm;
