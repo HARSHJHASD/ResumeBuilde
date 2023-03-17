7e1c59b57d3f1e7b955dc9d095c2b9229ebf43ec
masum <masum.r@antino.io>
2023-03-17 13:03:41 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './app.scss';
+import From from './components/Form'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <Form/>
+      <Navbar/>
+      <From/>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  {/* Container wrapper */}
+  <div className="container-fluid">
+    {/* Toggle button */}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarCenteredExample"
+      aria-controls="navbarCenteredExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars" />
+    </button>
+    {/* Collapsible wrapper */}
+    <div
+      className="collapse navbar-collapse justify-content-center"
+      id="navbarCenteredExample"
+    >
+      {/* Left links */}
+      <ul className="navbar-nav mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+      </ul>
+      {/* Left links */}
+    </div>
+    {/* Collapsible wrapper */}
+  </div>
+  {/* Container wrapper */}
+</nav>
+
+    </>
+  )
+}
+
+export default Navbar
