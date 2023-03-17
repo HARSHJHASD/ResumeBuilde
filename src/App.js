b263dec2d0d13acd9b54dab077a5f032d9924144
masum <masum.r@antino.io>
2023-03-17 13:11:55 +0530

    create navbar componet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './app.scss';
 import From from './components/Form'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <From/>
+      <Footer/>
     </div>
   )
 }
